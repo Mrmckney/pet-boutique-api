53254b733dffdd43f8785f2cfa4d8a3c66923a73
Matthew McKney <matthewmckney@Matthews-MacBook-Pro.local>
2021-07-12 17:04:20 -0400

    added more

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-const { getCustomers } = require('./src/customers')
+const { getCustomers, getCustomerById, getCustomerByQuery, createCustomer } = require('./src/customers')
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
+app.get('/customers/search', getCustomerByQuery)
+app.get('/customers/:id', getCustomerById)
 app.get('/customers', getCustomers)
-// app.post('/customers', createCustomer)
+
+app.post('/customers', createCustomer)
 
 app.listen(3000, () => {
     console.log('Listening to http://localhost:3000')
--- src/customers.js
+++ src/customers.js
@@ -1,4 +1,5 @@
 const admin = require('firebase-admin')
+
 const credentials = require('../credentials.json')
 
 function connectDb(){
@@ -21,4 +22,59 @@ exports.getCustomers = (request, response) => {
             console.error(err)
             response.status(500).send(err)
         })
+}
+
+exports.getCustomerById = (request, response) => {
+    if(!request.params.id){
+        response.status(400).send('No customer specified')
+        return
+    }
+    const db = connectDb()
+    db.collection('customers').doc(request.params.id).get()
+        .then(doc => {
+            const customer = doc.data()
+            customer.id = doc.id
+            response.send(customer)
+        })
+        .catch(err => {
+            console.error(err)
+            response.status(500).send(err)
+        })
+    
+}
+
+exports.getCustomerByQuery = (req, res) => {
+    // get query from re.query
+    const { fname } = req.query
+    // connect to fire store
+    const db = connectDb()
+    // search customers collection based on query
+    db.collection('customers').where('firstName', '==', fname).get()
+        // respond with results
+        .then(customerCollection => {
+            const matches = customerCollection.docs.map(doc => {
+                let customer = doc.data()
+                customer.id = doc.id
+                return customer
+            })
+            res.send(matches)
+        })
+        .catch(err => res.status(500).send(err))
+}
+
+// create post to createCustomer
+exports.createCustomer = (req, res) => {
+    const db = connectDb() // connect to db
+    
+    db.collection('customers')
+    .add(req.body)
+    .then(docRef => res.send(docRef.id))
+    .catch(err => {
+        console.log(err)
+        res.status(500).send('Customer could not be created')
+        
+    })
+
+
+
 }
